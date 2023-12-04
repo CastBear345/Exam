0f5ae75ab6568a7db12f797d752dcd53ca339516
Azim <Azim@DESKTOP-0997EST>
2023-12-04 20:00:17 +0600

    do

--- Exam/Program.cs
+++ Exam/Program.cs
@@ -6,6 +6,14 @@ public class Program
 {
     public static void Main()
     {
-        Menu.Start();
+        // Menu.Start();
+        Kitchen.GetKitchen(1,true);
+        ChildrensRoom.GetChildrensRoom(1,true);
+        BathRoom.GetBathRoom(1,true);
+        BadRoom.GetBedRoom(1,true);
+
+
+
+        Console.ReadKey();
     }
 }
